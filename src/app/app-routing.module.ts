919858db725742fb1aee7dc093e847ed1a182597
Andres Castillo Quesada <>
2020-11-25 10:32:20 -0600

    Basic nav just for modularity

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 import { StartingComponent } from './starting/starting.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'basic', component: StartingComponent },
 ];
 
